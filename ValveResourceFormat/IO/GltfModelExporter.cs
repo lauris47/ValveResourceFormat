0e410aa76afaeba763c0e9a496151546f6d25214
Pelle Johnsen <pelle.johnsen@skybox.gg>
2023-12-21 08:49:03 +0100

    refactor: Move glTF physics export to won file

--- ValveResourceFormat/IO/GltfModelExporter.cs
+++ ValveResourceFormat/IO/GltfModelExporter.cs
@@ -78,7 +78,7 @@ namespace ValveResourceFormat.IO
 #if DEBUG_VALIDATE_GLTF
 #pragma warning disable CS0168 // Variable is declared but never used
         private static ModelRoot debugCurrentExportedModel;
-        private static void DebugValidateGLTF()
+        public static void DebugValidateGLTF()
         {
             try
             {
@@ -91,7 +91,7 @@ namespace ValveResourceFormat.IO
             }
         }
 #else
-        private static void DebugValidateGLTF()
+        public static void DebugValidateGLTF()
         {
             // noop
         }
@@ -589,7 +589,7 @@ namespace ValveResourceFormat.IO
             }
 
 
-            var physNode = AddModelPhys(exportedModel, parentNode ?? scene, name,
+            var physNode = GltfPhysicsExporter.AddModelPhys(exportedModel, parentNode ?? scene, name,
                 loadedMeshDictionary, model);
             if (physNode != null)
             {
@@ -635,143 +635,6 @@ namespace ValveResourceFormat.IO
                 .Where(m => m.mesh != null);
 
             return embeddedMeshes.Concat(refMeshes);
-        }
-
-        private static Node AddModelPhys(ModelRoot exportedModel, IVisualNodeContainer parentNode, string name,
-            IDictionary<string, Mesh> loadedMeshDictionary,
-            VModel model = null)
-        {
-            var phys = model.GetEmbeddedPhys();
-            if (phys == null)
-            {
-                return null;
-            }
-
-            var physName = string.Concat(name, ".PHY");
-            var physNode = parentNode.CreateNode(physName);
-
-            var shapeTypeNames = new string[] { "Sphere", "Capsule", "Hull", "Mesh" };
-            int shapeTypeCount = shapeTypeNames.Length;
-            var groupCount = phys.CollisionAttributes.Count * shapeTypeCount;
-            var verts = new List<Vector3>[groupCount];
-            var inds = new List<int>[groupCount];
-            for (var i = 0; i < groupCount; i++)
-            {
-                verts[i] = new();
-                inds[i] = new();
-            }
-
-            CreatePhysGltfMeshes(phys, verts, inds);
-            for (var i = 0; i < groupCount; i++)
-            {
-                if (verts[i].Count < 1 || inds[i].Count < 1)
-                {
-                    continue;
-                }
-
-                var attributes = phys.CollisionAttributes[i / shapeTypeCount];
-                var tags = attributes.GetArray<string>("m_InteractAsStrings")
-                    ?? attributes.GetArray<string>("m_PhysicsTagStrings");
-                var group = attributes.GetStringProperty("m_CollisionGroupString");
-                var groupName = $"{string.Join("_", tags)}";
-                if (group != null)
-                {
-                    groupName = $"{groupName}{(groupName.Length > 1 ? "." : "")}{group}";
-                }
-
-                var physMeshName = string.Format("{0}.Mesh_{1}", physName, i);
-                loadedMeshDictionary.TryGetValue(physMeshName, out var mesh);
-                if (mesh == null)
-                {
-                    mesh = exportedModel.CreateMesh(physMeshName);
-                    mesh.Name = physMeshName;
-
-                    var primitive = mesh.CreatePrimitive();
-                    var accessors = new Dictionary<string, Accessor>();
-                    accessors["POSITION"] = CreateAccessor(exportedModel, verts[i].ToArray());
-                    foreach (var (attributeKey, accessor) in accessors)
-                    {
-                        primitive.SetVertexAccessor(attributeKey, accessor);
-                    }
-                    primitive.WithIndicesAccessor(PrimitiveType.TRIANGLES, inds[i]);
-
-                    loadedMeshDictionary.Add(physMeshName, mesh);
-                }
-                physNode.CreateNode($"{physName}.{shapeTypeNames[i % shapeTypeCount]}.{groupName}"
-                    ).WithMesh(mesh);
-            }
-
-            DebugValidateGLTF();
-            return physNode;
-        }
-
-        private static void CreatePhysGltfMeshes(PhysAggregateData phys, List<Vector3>[] verts, List<int>[] inds)
-        {
-            for (var p = 0; p < phys.Parts.Length; p++)
-            {
-                var shape = phys.Parts[p].Shape;
-                //TODO: Spheres
-                //TODO: Capsules
-                // Hulls
-                foreach (var hull in shape.Hulls)
-                {
-                    var collisionAttributeIndex = hull.CollisionAttributeIndex;
-                    collisionAttributeIndex = collisionAttributeIndex * 4 + 2;
-
-                    var vertOffset = verts[collisionAttributeIndex].Count;
-                    foreach (var v in hull.Shape.VertexPositions)
-                    {
-                        // if (bindPose.Any())
-                        // {
-                        //     v = Vector3.Transform(v, bindPose[p]);
-                        // }
-
-                        verts[collisionAttributeIndex].Add(v);
-                    }
-
-                    foreach (var face in hull.Shape.Faces)
-                    {
-                        var startEdge = face.Edge;
-                        for (var edge = hull.Shape.Edges[startEdge].Next; edge != startEdge;)
-                        {
-                            var nextEdge = hull.Shape.Edges[edge].Next;
-                            if (nextEdge == startEdge)
-                            {
-                                break;
-                            }
-
-                            inds[collisionAttributeIndex].Add(vertOffset + hull.Shape.Edges[startEdge].Origin);
-                            inds[collisionAttributeIndex].Add(vertOffset + hull.Shape.Edges[edge].Origin);
-                            inds[collisionAttributeIndex].Add(vertOffset + hull.Shape.Edges[nextEdge].Origin);
-                            edge = nextEdge;
-                        }
-                    }
-                }
-                // Mesh
-                foreach (var mesh in shape.Meshes)
-                {
-                    var collisionAttributeIndex = mesh.CollisionAttributeIndex;
-                    collisionAttributeIndex = collisionAttributeIndex * 4 + 3;
-
-                    var vertOffset = verts[collisionAttributeIndex].Count;
-                    foreach (var v in mesh.Shape.Vertices)
-                    {
-                        // if (bindPose.Any())
-                        // {
-                        //     v = Vector3.Transform(vec, bindPose[p]);
-                        // }
-
-                        verts[collisionAttributeIndex].Add(v);
-                    }
-
-                    foreach (var tri in mesh.Shape.Triangles)
-                    {
-                        inds[collisionAttributeIndex].Add(vertOffset + tri.Indices[0]);
-                        inds[collisionAttributeIndex].Add(vertOffset + tri.Indices[1]);
-                        inds[collisionAttributeIndex].Add(vertOffset + tri.Indices[2]);
-                    }
-                }
-            }
         }
 
         /// <summary>
@@ -881,7 +744,7 @@ namespace ValveResourceFormat.IO
             exportedModel.Save(filePath, settings);
         }
 
-        private static Accessor CreateAccessor(ModelRoot exportedModel, Vector2[] vectors)
+        public static Accessor CreateAccessor(ModelRoot exportedModel, Vector2[] vectors)
         {
             var bufferView = exportedModel.CreateBufferView(2 * sizeof(float) * vectors.Length, 0, BufferMode.ARRAY_BUFFER);
             new Vector2Array(bufferView.Content).Fill(vectors);
@@ -892,7 +755,7 @@ namespace ValveResourceFormat.IO
             return accessor;
         }
 
-        private static Accessor CreateAccessor(ModelRoot exportedModel, Vector3[] vectors)
+        public static Accessor CreateAccessor(ModelRoot exportedModel, Vector3[] vectors)
         {
             var bufferView = exportedModel.CreateBufferView(3 * sizeof(float) * vectors.Length, 0, BufferMode.ARRAY_BUFFER);
             new Vector3Array(bufferView.Content).Fill(vectors);
---
+++ ValveResourceFormat/IO/GltfPhysicsExporter.cs
@@ -0,0 +1,150 @@
+using System.Collections.Generic;
+using System.Numerics;
+using SharpGLTF.Schema2;
+using ValveResourceFormat.ResourceTypes;
+using ValveResourceFormat.Serialization;
+using Mesh = SharpGLTF.Schema2.Mesh;
+
+namespace ValveResourceFormat.IO
+{
+    public static class GltfPhysicsExporter
+    {
+
+        public static Node AddModelPhys(ModelRoot exportedModel, IVisualNodeContainer parentNode, string name,
+            IDictionary<string, Mesh> loadedMeshDictionary,
+            Model model = null)
+        {
+            var phys = model.GetEmbeddedPhys();
+            if (phys == null)
+            {
+                return null;
+            }
+
+            var physName = string.Concat(name, ".PHY");
+            var physNode = parentNode.CreateNode(physName);
+
+            var shapeTypeNames = new string[] { "Sphere", "Capsule", "Hull", "Mesh" };
+            int shapeTypeCount = shapeTypeNames.Length;
+            var groupCount = phys.CollisionAttributes.Count * shapeTypeCount;
+            var verts = new List<Vector3>[groupCount];
+            var inds = new List<int>[groupCount];
+            for (var i = 0; i < groupCount; i++)
+            {
+                verts[i] = new();
+                inds[i] = new();
+            }
+
+            CreatePhysGltfMeshes(phys, verts, inds);
+            for (var i = 0; i < groupCount; i++)
+            {
+                if (verts[i].Count < 1 || inds[i].Count < 1)
+                {
+                    continue;
+                }
+
+                var attributes = phys.CollisionAttributes[i / shapeTypeCount];
+                var tags = attributes.GetArray<string>("m_InteractAsStrings")
+                    ?? attributes.GetArray<string>("m_PhysicsTagStrings");
+                var group = attributes.GetStringProperty("m_CollisionGroupString");
+                var groupName = $"{string.Join("_", tags)}";
+                if (group != null)
+                {
+                    groupName = $"{groupName}{(groupName.Length > 1 ? "." : "")}{group}";
+                }
+
+                var physMeshName = string.Format("{0}.Mesh_{1}", physName, i);
+                loadedMeshDictionary.TryGetValue(physMeshName, out var mesh);
+                if (mesh == null)
+                {
+                    mesh = exportedModel.CreateMesh(physMeshName);
+                    mesh.Name = physMeshName;
+
+                    var primitive = mesh.CreatePrimitive();
+                    var accessors = new Dictionary<string, Accessor>();
+                    accessors["POSITION"] = GltfModelExporter.CreateAccessor(exportedModel, verts[i].ToArray());
+                    foreach (var (attributeKey, accessor) in accessors)
+                    {
+                        primitive.SetVertexAccessor(attributeKey, accessor);
+                    }
+                    primitive.WithIndicesAccessor(PrimitiveType.TRIANGLES, inds[i]);
+
+                    loadedMeshDictionary.Add(physMeshName, mesh);
+                }
+                physNode.CreateNode($"{physName}.{shapeTypeNames[i % shapeTypeCount]}.{groupName}"
+                    ).WithMesh(mesh);
+            }
+
+            GltfModelExporter.DebugValidateGLTF();
+            return physNode;
+        }
+
+        private static void CreatePhysGltfMeshes(PhysAggregateData phys, List<Vector3>[] verts, List<int>[] inds)
+        {
+            for (var p = 0; p < phys.Parts.Length; p++)
+            {
+                var shape = phys.Parts[p].Shape;
+                //TODO: Spheres
+                //TODO: Capsules
+                // Hulls
+                foreach (var hull in shape.Hulls)
+                {
+                    var collisionAttributeIndex = hull.CollisionAttributeIndex;
+                    collisionAttributeIndex = collisionAttributeIndex * 4 + 2;
+
+                    var vertOffset = verts[collisionAttributeIndex].Count;
+                    foreach (var v in hull.Shape.VertexPositions)
+                    {
+                        // if (bindPose.Any())
+                        // {
+                        //     v = Vector3.Transform(v, bindPose[p]);
+                        // }
+
+                        verts[collisionAttributeIndex].Add(v);
+                    }
+
+                    foreach (var face in hull.Shape.Faces)
+                    {
+                        var startEdge = face.Edge;
+                        for (var edge = hull.Shape.Edges[startEdge].Next; edge != startEdge;)
+                        {
+                            var nextEdge = hull.Shape.Edges[edge].Next;
+                            if (nextEdge == startEdge)
+                            {
+                                break;
+                            }
+
+                            inds[collisionAttributeIndex].Add(vertOffset + hull.Shape.Edges[startEdge].Origin);
+                            inds[collisionAttributeIndex].Add(vertOffset + hull.Shape.Edges[edge].Origin);
+                            inds[collisionAttributeIndex].Add(vertOffset + hull.Shape.Edges[nextEdge].Origin);
+                            edge = nextEdge;
+                        }
+                    }
+                }
+                // Mesh
+                foreach (var mesh in shape.Meshes)
+                {
+                    var collisionAttributeIndex = mesh.CollisionAttributeIndex;
+                    collisionAttributeIndex = collisionAttributeIndex * 4 + 3;
+
+                    var vertOffset = verts[collisionAttributeIndex].Count;
+                    foreach (var v in mesh.Shape.Vertices)
+                    {
+                        // if (bindPose.Any())
+                        // {
+                        //     v = Vector3.Transform(vec, bindPose[p]);
+                        // }
+
+                        verts[collisionAttributeIndex].Add(v);
+                    }
+
+                    foreach (var tri in mesh.Shape.Triangles)
+                    {
+                        inds[collisionAttributeIndex].Add(vertOffset + tri.Indices[0]);
+                        inds[collisionAttributeIndex].Add(vertOffset + tri.Indices[1]);
+                        inds[collisionAttributeIndex].Add(vertOffset + tri.Indices[2]);
+                    }
+                }
+            }
+        }
+    }
+}
